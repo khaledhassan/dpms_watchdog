6a376482956a7e26c8da4e958e03cde2208d4fa2
Khaled Hassan <khaled.hassan@gmail.com>
2021-08-28 21:03:50 -0400

    adjust sleep time, move sleep to top of lhe loop so that the setting can take effect sooner

--- dpms_watchdog.sh
+++ dpms_watchdog.sh
@@ -5,11 +5,12 @@ set -e
 DPMS_STANDBY_TIME=${DPMS_STANDBY_TIME:-300}
 DPMS_SUSPEND_TIME=${DPMS_SUSPEND_TIME:-300}
 DPMS_OFF_TIME=${DPMS_OFF_TIME:-300}
-DPMS_WATCHDOG_SLEEP_TIME=${DPMS_WATCHDOG_SLEEP_TIME:-300}
+DPMS_WATCHDOG_SLEEP_TIME=${DPMS_WATCHDOG_SLEEP_TIME:-60}
 
 while true
 do
+	sleep "$DPMS_WATCHDOG_SLEEP_TIME" # sleep first as the setting appears to get cleared upon login/unlock;
+	                                  # this should allow the setting to take place sooner, as the unlock happens before the sleep is finished
 	xset dpms "$DPMS_STANDBY_TIME" "$DPMS_SUSPEND_TIME" "$DPMS_OFF_TIME"
-	sleep "$DPMS_WATCHDOG_SLEEP_TIME"
 done
 
